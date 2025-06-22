d8859a38a7ae68deb2f45c390672551b2be17505
khadar099 <khadar3099@gmail.com>
2025-06-22 07:51:51 +0530

    first commit , pusing to remote

---
+++ src/main/java/com/PrelovedHub/PrelovedHubApplication.java
@@ -0,0 +1,13 @@
+package com.PrelovedHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrelovedHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrelovedHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PrelovedHub/PrelovedHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PrelovedHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrelovedHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
